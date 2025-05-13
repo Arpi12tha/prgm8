ff8e51ca8f0d2ac8fa072e893ab89d7bcd31a29a
Arpitha Bekal <arpithabekal04@gmail.com>
2025-05-13 06:10:22 -0700

    first commit

---
+++ prgm8/src/main/java/com/Arpi12tha/App.java
@@ -0,0 +1,13 @@
+package com.Arpi12tha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
